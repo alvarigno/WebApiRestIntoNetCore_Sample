10538edd912461d8d2a7e53daaa770ecf9ee2523
Álvaro Emparán <alvaro.emparan@gmail.com>
2019-03-30 18:35:50 -0300

    update controllers

--- WebApiRestIntoNetCore_Sample/WebApiRestIntoNetCore_Sample/Controllers/ValuesController.cs
+++ WebApiRestIntoNetCore_Sample/WebApiRestIntoNetCore_Sample/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace WebApiRestIntoNetCore_Sample.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "value"+id;
         }
 
         // POST api/values
